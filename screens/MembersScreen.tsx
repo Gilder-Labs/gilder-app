264c1f7c75ace36fe853164522b12122912dffb1
Noah <noah.drabinsky1@gmail.com>
2022-02-05 10:13:14 -0700

    Render members in flatlist

--- screens/MembersScreen.tsx
+++ screens/MembersScreen.tsx
@@ -2,36 +2,35 @@ import { RootTabScreenProps } from "../types";
 import styled from "styled-components/native";
 import { useAppSelector } from "../hooks/redux";
 import { MemberCard } from "../components";
+import { FlatList } from "react-native";
 
 export default function ActivityScreen({
   navigation,
 }: RootTabScreenProps<"Activity">) {
   const { realmMembers } = useAppSelector((state) => state.realms);
 
+  const renderMember = ({ item }: any) => {
+    return <MemberCard key={item.governingTokenOwner} member={item} />;
+  };
+
   return (
-    <ScrollableContainer>
-      <Container>
+    <Container>
+      {/* <Container>
         {realmMembers.map((member) => (
           <MemberCard key={member.governingTokenOwner} member={member} />
         ))}
-      </Container>
-    </ScrollableContainer>
+      </Container> */}
+      <FlatList
+        data={realmMembers}
+        renderItem={renderMember}
+        keyExtractor={(item) => item.governingTokenOwner}
+      />
+    </Container>
   );
 }
-
-const ScrollableContainer = styled.ScrollView`
-  background-color: ${(props) => props.theme.gray[900]};
-  flex: 1;
-  padding-top: ${(props) => props.theme.spacing[2]};
-`;
 
 const Container = styled.View`
   background-color: ${(props) => props.theme.gray[900]};
   flex: 1;
-  padding: ${(props) => props.theme.spacing[2]};
-  flex-wrap: wrap;
-  width: 100%;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: stretch;
+  padding-top: ${(props) => props.theme.spacing[2]};
 `;
