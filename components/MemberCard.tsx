ff49faab734e8721d031319f5bf09d8d7e6eb051
Noah <noah.drabinsky1@gmail.com>
2022-02-09 18:26:57 -0700

    Base structure of member card

--- components/MemberCard.tsx
+++ components/MemberCard.tsx
@@ -5,6 +5,8 @@ import { SvgXml } from "react-native-svg";
 import { useTheme } from "styled-components";
 import { createAvatar } from "@dicebear/avatars";
 import * as style from "@dicebear/avatars-jdenticon-sprites";
+import { LinearGradient } from "expo-linear-gradient";
+import * as Unicons from "@iconscout/react-native-unicons";
 
 interface MemberCardProps {
   member: any;
@@ -20,42 +22,59 @@ export const MemberCard = ({ member }: MemberCardProps) => {
 
   return (
     <Container>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={[`${theme.primary[600]}44`, `${theme.primary[800]}44`]}
+        style={{ height: 48, flex: 1, width: "100%" }}
+        start={{ x: 0.1, y: 0.2 }}
+      ></LinearGradient>
       <IconContainer>
-        <SvgXml xml={jdenticonSvg} width="36px" height="36px" />
+        <SvgXml xml={jdenticonSvg} width="44px" height="44px" />
+      </IconContainer>
+      <ContentContainer>
+        <MemberName>{`${member.governingTokenOwner.slice(
+          0,
+          4
+        )}...${member.governingTokenOwner.slice(0, 4)}`}</MemberName>
+
         <TextContainer>
           <VotesCast>Total Votes Cast: {member.totalVotesCount}</VotesCast>
           <VotesCast>Vote weight: {member.depositAmount}</VotesCast>
         </TextContainer>
-      </IconContainer>
-      <MemberName>{`${member.governingTokenOwner.slice(
-        0,
-        4
-      )}...${member.governingTokenOwner.slice(0, 4)}`}</MemberName>
+      </ContentContainer>
     </Container>
   );
 };
 
+//           <VotesCast>Vote weight: {member.depositAmount}</VotesCast>
+
 const Container = styled.View`
-  height: 80px;
-  width: 100%%;
   margin-bottom: ${(props: any) => props.theme.spacing[3]};
   border-radius: 4px;
   background: ${(props: any) => props.theme.gray[800]};
-  padding-left: ${(props: any) => props.theme.spacing[4]};
-  padding-right: ${(props: any) => props.theme.spacing[4]};
-  flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  position: relative;
+`;
+
+const ContentContainer = styled.View`
+  padding: ${(props: any) => props.theme.spacing[5]};
+  width: 100%;
+  /* justify-content: space-between; */
 `;
 
 const MemberName = styled.Text`
   color: ${(props: any) => props.theme.gray[100]}
   font-weight: bold;
+  font-size: 18px;
+  margin-top:${(props: any) => props.theme.spacing[2]};
+  margin-bottom: ${(props: any) => props.theme.spacing[3]};
 
 `;
 
 const TextContainer = styled.View`
-  margin-left: ${(props: any) => props.theme.spacing[2]};
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 const VotesCast = styled.Text`
@@ -64,6 +83,13 @@ const VotesCast = styled.Text`
 
 const IconContainer = styled.View`
   /* border-radius: 100px, */
+  background: ${(props: any) => props.theme.primary[800]};
   flex-direction: row;
   align-items: center;
+  position: absolute;
+  left: 20px;
+  top: 24px;
+  overflow: hidden;
+  border: 2px solid black;
+  border-radius: 100px;
 `;
