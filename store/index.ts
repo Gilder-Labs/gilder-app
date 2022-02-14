45339239a4c941a02c081038270505e45ed400b3
Noah <noah.drabinsky1@gmail.com>
2022-02-14 10:12:05 -0700

    Fix redux persist

--- store/index.ts
+++ store/index.ts
@@ -13,10 +13,12 @@ import {
   REGISTER,
 } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
+  stateReconciler: autoMergeLevel2,
 };
 
 const rootReducer = combineReducers({
@@ -36,8 +38,6 @@ export const store = configureStore({
       },
     }),
 });
-
-let persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
