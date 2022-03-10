b3bf189c5d6debb4e4e899c492998f99814be967
Noah <noah.drabinsky1@gmail.com>
2022-03-10 14:44:22 -0700

    Public key util

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,7 @@
 export * from "./cleanData";
 export * from "./setupWalletConnect";
 export * from "./member";
+
+export const abbreviatePublicKey = (publicKey: string) => {
+  return `${publicKey.slice(0, 4)}...${publicKey.slice(-4)}`;
+};
