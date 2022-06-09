e94470e51bb345cb06923f82e88309ef3775049b
Noah Drabinsky <noah.drabinsky@gmail.com>
2022-06-09 15:44:40 -0600

    update app version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Gilder",
     slug: "gilder-app",
-    version: "1.0.16",
+    version: "1.0.17",
     orientation: "portrait",
     icon: "./assets/images/DarkIcon.png",
     scheme: "gilder",
