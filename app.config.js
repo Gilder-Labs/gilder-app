2d223a690d98a188989f4d836d1c8d8c0f5ef3c5
Noah Drabinsky <noah.drabinsky@gmail.com>
2022-07-26 10:11:54 -0600

    update version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Gilder",
     slug: "gilder-app",
-    version: "1.0.28",
+    version: "1.0.29",
     orientation: "portrait",
     icon: "./assets/images/DarkIcon.png",
     scheme: "gilder",
@@ -29,7 +29,7 @@ export default {
         foregroundImage: "./assets/images/DarkIconAndroid.png",
         backgroundColor: "#000000",
       },
-      version: "1.0.28",
+      version: "1.0.29",
       googleServicesFile: "./google-services.json",
       package: "com.dawggydawg.gilderapp",
     },
