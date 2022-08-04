6f3651a23f603ded9ca1c75216e4c02a40bf68f3
ndrabins <noah.drabinsky1@gmail.com>
2022-08-04 12:13:28 -0600

    Fix metro config

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,7 @@ const { getDefaultConfig } = require("@expo/metro-config");
 const config = getDefaultConfig(__dirname);
 
 config.transformer = {
+  ...config.transformer,
   babelTransformerPath: require.resolve("react-native-svg-transformer"),
   getTransformOptions: () => ({
     transform: {
@@ -13,6 +14,7 @@ config.transformer = {
 };
 
 config.resolver = {
+  ...config.resolver,
   assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
   sourceExts: [
     ...config.resolver.sourceExts,
