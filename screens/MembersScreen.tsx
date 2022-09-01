27a7071e0b63d1850ed9711911479e65726ebcca
ndrabins <noah.drabinsky1@gmail.com>
2022-09-01 12:03:18 -0600

    Remove unecessary copy

--- screens/MembersScreen.tsx
+++ screens/MembersScreen.tsx
@@ -90,7 +90,7 @@ export default function MemberScreen({
       );
     }
 
-    setFilteredAndSortedMembers([...sortedMembers]);
+    setFilteredAndSortedMembers(sortedMembers);
     setSortType(value);
   };
 
