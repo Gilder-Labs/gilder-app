5616689a94d80cd7d74837fbcf89d49ca46dcc9a
ndrabins <noah.drabinsky1@gmail.com>
2022-09-02 08:39:44 -0600

    update app version for ios

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -129,7 +129,7 @@ function DrawerScreen() {
 
   return (
     <Drawer.Navigator
-      initialRouteName="Members"
+      initialRouteName="Proposal"
       drawerContent={(props) => <DrawerContentContainer {...props} />}
       screenOptions={{
         drawerActiveBackgroundColor: `${theme?.gray[800]}aa`,
