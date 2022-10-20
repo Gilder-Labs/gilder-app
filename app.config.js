4541900480d2ea23c277b5e8cc0e1445ffcbf79a
ndrabins <noah.drabinsky1@gmail.com>
2022-10-20 10:40:43 -0600

    Update version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Gilder",
     slug: "gilder-app",
-    version: "1.0.58",
+    version: "1.0.59",
     orientation: "portrait",
     icon: "./assets/images/DarkIcon.png",
     scheme: "gilder",
@@ -24,14 +24,14 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.dawggydawg.gilderapp",
-      buildNumber: "27",
+      buildNumber: "28",
     },
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/images/DarkIconAndroid.png",
         backgroundColor: "#000000",
       },
-      versionCode: 27,
+      versionCode: 28,
       googleServicesFile: "./google-services.json",
       package: "com.dawggydawg.gilderapp",
     },
