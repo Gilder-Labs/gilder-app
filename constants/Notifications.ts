803f6d3d29728e2bbacfec8eebf1388dcc6bcd46
Ryan Szemplinski <ryan.szemplinski@pieinsurance.com>
2022-11-07 12:47:40 -0700

    Changing url

--- constants/Notifications.ts
+++ constants/Notifications.ts
@@ -5,4 +5,4 @@ export const USE_LOCAL_NOTIFICATION_API =
 
 export const NOTIFICATION_API_URL = USE_LOCAL_NOTIFICATION_API
   ? `http://${Constants.manifest?.debuggerHost?.split(":").shift()}:3001`
-  : "https://api.gilder.xyz/notify";
+  : "https://notify.gilder.xyz";
